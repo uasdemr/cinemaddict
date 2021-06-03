e9e36b030614b3b216b579f0e97fdbd06d387ef6
Serge <sdyuzhilin@mail.ru>
2021-06-03 10:03:38 +0300

    Настройка сервера для разработки

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require(`path`);
+
+module.exports = {
+  mode: `development`,
+  entry: `./src/main.js`,
+  output: {
+    filename: `bundle.js`,
+    path: path.resolve(__dirname, `public`),
+  },
+  devtool: `source-map`,
+  devServer: {
+    contentBase: path.resolve(__dirname, `public`),
+    watchContentBase: true,
+  },
+  module: {
+    rules: [
+        {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader']
+        }
+    ]
+  }
+};
